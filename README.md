6bd91105b5f23575fc29e345842b772c0546454b
ardizanki2919 <setiawanardi2024>
2025-01-26 10:06:17 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+## Evoluz Service Request Feature Testing
+
+This repository contains tests for the **Tambah Permintaan Pelayanan** feature on the **Evoluz** website.
+
+### Testing Framework
+
+The tests are written using **Playwright**, a powerful framework for end-to-end testing in modern web applications.
+
+### Test Cases
+
+You can view the test cases created for this feature on the following [Google Sheets](https://docs.google.com/spreadsheets/d/1YAp2CHZVO0tag3jwXdcIoClVVWARzcSRcB0VOZCsKp4/edit?usp=sharing).
+
+### How to Use
+
+Follow the steps below to set up and run the tests:
+
+#### 1. Clone the Repository
+
+First, clone this repository to your local machine.
+
+```bash
+git clone <repository-url>
+```
+
+#### 2. Install Dependencies
+
+Navigate to the project directory and install the required dependencies using npm.
+
+```bash
+cd <project-directory>
+npm install
+```
+
+#### 3. Set Up Environment Variables
+
+Create a .env file in the root directory, and set its values based on the provided .env.example file.
+
+#### 4. Running Playwright Tests
+
+To run the Playwright tests, use the following command:
+
+```bash
+npx playwright test
+```
+
+```bash
+npx playwright show-report
+```
+
+or
+
+Run tests in UI mode
+```bash
+npx playwright test --ui
+```
+
+While the tests cover the essential features, there are still areas that need optimization:
+
+- **Timeout**
+- **Locator**
+- **Scrolling**
+- **Data Import**
+
+### License
+
+1. You are free to use this code as inspiration.
+2. Please do not copy it directly.
+3. Crediting the author is appreciated.
---
+++ tests/request.spec.ts
@@ -0,0 +1,162 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const baseUrl = process.env.BASE_URL;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+
+if (!baseUrl || !username || !password) {
+  throw new Error('Please set BASE_URL, USERNAME, and PASSWORD in your .env file.');
+}
+
+const login = async (page) => {
+  await page.goto(`${baseUrl}/account/login`);
+  await page.locator('#nameOrEmail').fill(username || '');
+  await page.locator('#password').fill(password || '');
+  await page.getByRole('button', { name: '󰍂 Masuk' }).click();
+  
+  await page.waitForTimeout(6000);
+  await expect(page).toHaveURL(`${baseUrl}/dashboard/project`);
+};
+
+const navigateToRequestPage = async (page) => {
+    await page.getByRole('link', { name: ' Permintaan Pelayanan' }).click();
+    await page.waitForTimeout(6000);
+    await page.waitForURL(`${baseUrl}/apps/request-service`);
+};
+
+const addService = async (page, title = '', details = '') => {
+    await page.getByRole('button', { name: ' Tambah Permintaan Pelayanan' }).click();
+    await page.waitForTimeout(6000);
+    await expect(page).toHaveURL(`${baseUrl}/apps/request-service/add`);
+
+    await page.locator('#billing_requestService').fill(title);
+    await page.locator('.ck-placeholder').fill(details);
+
+    await page.getByRole('button', { name: ' Kirim' }).click();
+    await page.waitForTimeout(30000);
+};
+
+const handleWaitingAction = async (page, action, reason = '') => {
+    await page.getByRole('button', { name: 'waiting' }).click()
+    await page.waitForTimeout(30000);
+
+    switch (action) {
+        case 'accept':
+            await page.getByRole('button', { name: 'Ya, Terima' }).click();
+            break;
+        
+        case 'consider':
+            await page.getByRole('button', { name: 'Dipertimbangkan' }).click();
+            await page.locator('#considerReason"]').fill(reason);
+            break;
+
+        case 'reject':
+            await page.getByRole('button', { name: 'Tidak' }).click();
+            await page.locator('#rejectionReason"]').fill(reason);
+            break;
+
+    default:
+      throw new Error(`Invalid action: ${action}`);
+    };
+
+    await page.getByRole('button', { name: ' Ubah' }).click();
+    await page.waitForTimeout(30000);
+};
+
+const search = async (page, keywords = '') => {
+    await page.locator('#search').fill(keywords);
+    await page.waitForTimeout(6000);
+  };
+
+test.describe('Permintaan Pelayanan', () => {
+  test('Login dengan kredensial valid', async ({ page }) => {
+    await login(page);
+  });
+
+  test('Menambahkan permintaan pelayanan dan mengubah status menjadi Accepted', async ({ page }) => {
+    await login(page);
+    await navigateToRequestPage(page);
+    await addService(
+        page,
+        'Judul Permintaan Test CCTV 5',
+        'Detail permintaan test CCTV terbaru'
+    );
+
+    await handleWaitingAction(page, 'accept');
+
+    await page.getByRole('button', { name: '󰁍 Kembali' }).click();
+    await page.waitForTimeout(6000);
+    await expect(page).toHaveURL(`${baseUrl}/apps/request-service`);
+  });
+
+  test('Menambahkan permintaan pelayanan dan mengubah status menjadi Rejected', async ({ page }) => {
+    await login(page);
+    await navigateToRequestPage(page);
+    await addService(
+        page,
+        'Judul Permintaan Test CCTV 6',
+        'Detail permintaan test CCTV terbaru'
+    );
+
+    await handleWaitingAction(page, 'reject', 'pengajuan ditolak');
+
+    await page.getByRole('button', { name: '󰁍 Kembali' }).click();
+    await page.waitForTimeout(6000);
+    await expect(page).toHaveURL(`${baseUrl}/apps/request-service`);
+  });
+
+  test('Menambahkan permintaan pelayanan dan mengubah status menjadi Considered', async ({ page }) => {
+    await login(page);
+    await navigateToRequestPage(page);
+    await addService(
+        page,
+        'Judul Permintaan Test CCTV 7',
+        'Detail permintaan test CCTV terbaru'
+    );
+
+    await handleWaitingAction(page, 'consider', 'pengajuan ditangguhkan');
+
+    await page.getByRole('button', { name: '󰁍 Kembali' }).click();
+    await page.waitForTimeout(6000);
+    await expect(page).toHaveURL(`${baseUrl}/apps/request-service`);
+  });
+
+  test('Menambah permintaan pelayanan dengan hanya mengisi judul', async ({ page }) => {
+    await login(page);
+    await navigateToRequestPage(page);
+    await addService(
+        page,
+        'Judul Permintaan Test CCTV 5'
+    );
+
+    await handleWaitingAction(page, 'accept');
+
+    await page.getByRole('button', { name: '󰁍 Kembali' }).click();
+    await page.waitForTimeout(6000);
+    await expect(page).toHaveURL(`${baseUrl}/apps/request-service`);
+  });
+
+  test('Menambah permintaan pelayanan tanpa mengisi judul', async ({ page }) => {
+    await login(page);
+    await navigateToRequestPage(page);
+    await addService(
+        page
+    );
+  });
+
+  test('Melakukan pencarian dengan data valid yang tersedia', async ({ page }) => {
+    await login(page);
+    await navigateToRequestPage(page);
+    await search(page, 'Judul Permintaan Test CCTV 3');
+  });
+
+  test('Melakukan pencarian dengan data valid yang tidak tersedia', async ({ page }) => {
+    await login(page);
+    await navigateToRequestPage(page);
+    await search(page, 'ABC 123 456');
+    await expect(page.getByRole('cell', { name: 'Data Tidak Ada' })).toBeVisible();
+  });
+});
