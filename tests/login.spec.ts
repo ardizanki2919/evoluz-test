34fb5666003653903e8f1a0e065c8d0cde384ecc
ardizanki2919 <setiawanardi2024@gmail.com>
2025-02-07 09:46:31 +0700

    Add login test spec

---
+++ tests/login.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { login, logout } from '../modules/login';
+import { baseUrl, username, password } from '../utils/config.js';
+import { loginData } from '../data/loginData.js';
+
+test.describe('Login', () => {
+  test('Login dengan kredensial valid', async ({ page }) => {
+    await login(page, username, password);
+    await expect(page).toHaveURL(`${baseUrl}/dashboard/project`);
+  });
+
+  test('Login dengan username valid tetapi password tidak valid', async ({ page }) => {
+    await login(page, loginData.invalidData[0].username, loginData.invalidData[0].password);
+  });
+
+  test('Login dengan username tidak valid dan password valid', async ({ page }) => {
+    await login(page, loginData.invalidData[1].username, loginData.invalidData[1].password);
+  });
+
+  test('Login tanpa mengisi data username dan password', async ({ page }) => {
+    await login(page, loginData.invalidData[2].username, loginData.invalidData[2].password);
+    await expect(page.getByText('Masukkan Username atau Email')).toBeVisible();
+    await expect(page.getByText('Masukkan Kata Sandi')).toBeVisible();
+  });
+
+  test('Logout dari aplikasi', async ({ page }) => {
+    await logout(page, username, password);
+    await expect(page.getByRole('heading', { name: 'See You Again !' })).toBeVisible();
+    await expect(page.getByText('You are now successfully sign')).toBeVisible();
+  });
+});
